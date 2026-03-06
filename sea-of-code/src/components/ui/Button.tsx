27f3e5ed0a7bc4d18a78089f7efa520e9278abc5
maksim.zhidkov <vjmax793@gmail.com>
2026-03-06 19:48:40 +0300

    feat: add optional button start icon

--- sea-of-code/src/components/ui/Button.tsx
+++ sea-of-code/src/components/ui/Button.tsx
@@ -1,22 +1,32 @@
-import type { ButtonHTMLAttributes, FC } from 'react';
+import type { ButtonHTMLAttributes, FC, ReactNode } from 'react';
 
 type ButtonVariant = 'primary' | 'secondary';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: ButtonVariant;
+  icon?: ReactNode;
 }
 
 const variantStyles: Record<ButtonVariant, string> = {
   primary: 'bg-indigo-500 text-white hover:bg-indigo-400 focus-visible:outline-indigo-600',
-  secondary: 'bg-gray-100 text-gray-900 hover:bg-gray-200 focus-visible:outline-gray-400',
+  secondary: 'bg-stone-500 hover:bg-gray-200 focus-visible:outline-gray-400',
 };
 
-const Button: FC<ButtonProps> = ({ variant = 'primary', className = '', ...props }) => {
+const Button: FC<ButtonProps> = ({
+  variant = 'primary',
+  icon,
+  className = '',
+  children,
+  ...props
+}) => {
   return (
     <button
-      className={`flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 ${variantStyles[variant]} ${className}`}
+      className={`flex w-full cursor-pointer items-center justify-center gap-2 rounded-md px-3 py-1.5 text-sm font-semibold shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 ${variantStyles[variant]} ${className}`}
       {...props}
-    />
+    >
+      {icon}
+      {children}
+    </button>
   );
 };
 
