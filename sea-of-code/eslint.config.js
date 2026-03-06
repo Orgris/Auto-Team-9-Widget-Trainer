f20e32524940a81441ef231ac0c5a10e0342d04e
maksim.zhidkov <vjmax793@gmail.com>
2026-03-06 19:23:28 +0300

    feat: add Prettier + ESLint extensions support

--- sea-of-code/eslint.config.js
+++ sea-of-code/eslint.config.js
@@ -3,21 +3,25 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules']),
+
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.ts', '**/*.tsx'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ...tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier
     ],
+
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-  },
+      ecmaVersion: 2022,
+      globals: globals.browser
+    }
+  }
 ]);
